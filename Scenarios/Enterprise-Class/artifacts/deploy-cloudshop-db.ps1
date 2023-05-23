73e5256b512fa3b22d3f8219ea5c449ca0e34dd8
YRollHid <yans.roll@gmail.com>
2023-05-23 10:52:31 +0200

    Update artifact for SQLVM

--- Scenarios/Enterprise-Class/artifacts/deploy-cloudshop-db.ps1
+++ Scenarios/Enterprise-Class/artifacts/deploy-cloudshop-db.ps1
@@ -5,11 +5,11 @@ $data    = "C:\Data"
 $backups = "C:\Backup" 
 $script  = "C:\Script" 
 
-[system.io.directory]::CreateDirectory($logs)
-[system.io.directory]::CreateDirectory($data)
-[system.io.directory]::CreateDirectory($backups)
-[system.io.directory]::CreateDirectory($script)
-[system.io.directory]::CreateDirectory("C:\SQLDATA")
+New-Item -ItemType Directory -Path $logs
+New-Item -ItemType Directory -Path $data
+New-Item -ItemType Directory -Path $backups
+New-Item -ItemType Directory -Path $script
+New-Item -ItemType Directory -Path "C:\SQLDATA"
 
 $splitpath = $sqlConfigUrl.Split("/")
 $fileName = $splitpath[$splitpath.Length-1]
@@ -30,3 +30,34 @@ Invoke-Command -FilePath $destinationPath -Credential $credential -ComputerName 
 Disable-PSRemoting -Force
 
 New-NetFirewallRule -DisplayName "SQL Server" -Direction Inbound –Protocol TCP –LocalPort 1433 -Action allow 
+
+# Disable IE Enhanced Security Configuration
+$AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+$UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+
+New-Item -Path $adminKey -Force
+New-Item -Path $UserKey -Force
+New-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+New-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+$HKCU = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4"
+Set-ItemProperty -Path $HKLM -Name "1803" -Value 0
+Set-ItemProperty -Path $HKCU -Name "1803" -Value 0
+$HKLM = "HKLM:\Software\Microsoft\Internet Explorer\Security"
+New-ItemProperty -Path $HKLM -Name "DisableSecuritySettingsCheck" -Value 1 -PropertyType DWORD
+
+Stop-Process -Name Explorer
+Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
